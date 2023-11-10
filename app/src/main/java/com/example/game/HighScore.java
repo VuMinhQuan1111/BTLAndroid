3f9956abc42fd8719906bad45d7d9e23ac98b09d
your name <abc@gmail.com>
2023-11-11 01:27:50 +0700

    bxh

--- app/src/main/java/com/example/game/HighScore.java
+++ app/src/main/java/com/example/game/HighScore.java
@@ -3,17 +3,34 @@ package com.example.game;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class HighScore extends Activity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("Score");
 
+
+    ListView listScore;
     private TextView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +39,7 @@ public class HighScore extends Activity {
 
         back = findViewById(R.id.back_btn);
 
-
+        Anhxa();
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,35 +48,52 @@ public class HighScore extends Activity {
                 startActivity(intent);
             }
         });
+        //cau hinh listview
+        final ArrayList<String> mang = new ArrayList<>();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                mang);
+        listScore.setAdapter(adapter);
 
-//        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
-//
-//        bottomNavigationView.setSelectedItemId(R.id.action_score);
-//
-//        //nhấp chuột vào 3 icon ở dưới cùng
-//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                switch(item.getItemId()){
-//
-//                    case R.id.action_home:
-//                        startActivity(new Intent(getApplicationContext()
-//                                ,MainActivity.class));
-//                        overridePendingTransition(0,0);
-//                        return true;
-//
-//                    case R.id.action_score:
-//                        return true;
-//
-//                    case R.id.action_user:
-//                        startActivity(new Intent(getApplicationContext()
-//                                ,User.class));
-//                        overridePendingTransition(0,0);
-//                        return true;
-//
-//                }
-//                return false;
-//            }
-//        });
+        // Bat su kien
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mang.clear(); // Xóa danh sách cũ trước khi cập nhật mới
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    if (snapshot.hasChild("name") && snapshot.hasChild("score")) {
+                        String name = snapshot.child("name").getValue().toString();
+                        String score = snapshot.child("score").getValue().toString();
+                        String item = name + ": " + score;
+                        mang.add(item);
+                    }
+                }
+
+//                // Sắp xếp danh sách theo điểm số
+                Collections.sort(mang, new Comparator<String>() {
+                    @Override
+                    public int compare(String s1, String s2) {
+                        // Sắp xếp theo điểm số giảm dần
+                        int score1 = Integer.parseInt(s1.split(":")[1].trim());
+                        int score2 = Integer.parseInt(s2.split(":")[1].trim());
+                        return Integer.compare(score2, score1);
+                    }
+                });
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Xử lý khi có lỗi
+            }
+        });
+
+
+    }
+
+
+    public void Anhxa(){
+        listScore = (ListView)  findViewById(R.id.listView);
     }
 }
